63f34ad62ce2747fb480ebf5f3176b5a4439fae5
nwtang <nick.w.tang@gmail.com>
2020-07-17 17:51:51 -0700

    update

---
+++ fade.js
@@ -0,0 +1,16 @@
+(function() {
+
+    var titles = $(".titles");
+    var titleIndex = -1;
+    
+    function showNextQuote() {
+        ++titleIndex;
+        titles.eq(titleIndex % titles.length)
+            .fadeIn(2000)
+            .delay(2000)
+            .fadeOut(2000, showNextQuote);
+    }
+    
+    showNextQuote();
+    
+})();
